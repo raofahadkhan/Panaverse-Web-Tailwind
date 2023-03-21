19016cfa8421f5db19e105919adca812afbc8006
RAO FAHAD <raofahad046@gmail.com>
2023-03-22 01:20:42 +0500

    Minor ui fix

--- src/components/widgets/SpecializedBox.tsx
+++ src/components/widgets/SpecializedBox.tsx
@@ -18,9 +18,7 @@ const SpecializedBox = () => {
             select one or more specializations consisting of two courses each.
           </p>
         </div>
-        {/* <div className="mt-14  flex flex-col md:flex-row gap-y-10 md:gap-y-10 lg:space-y-0 lg:gap-y-10 md:space-x-7 items-stretch md:justify-center flex-wrap "> */}
-        {/* <div className="mt-14 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3  md:flex-row mx-4 md:space-x-7 place-items-stretch  "> */}
-        {/* <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 lg:gap-y-12 lg:gap-x-4 md:gap-y-10 md:gap-x-4 w-fit mx-auto"> */}
+
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-y-10 lg:gap-y-12 lg:gap-x-9 md:gap-y-10 md:gap-x-4 w-fit mx-auto ">
           {specializedBoxData.map((data: SpecializedBoxData, index: number) => (
             <Box
